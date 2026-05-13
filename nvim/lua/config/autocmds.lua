8aa31f43a47ebd9624dc5b8a5a43c2d66bc78900
Oscar <omsellors98@gmail.com>
2026-05-13 15:50:54 +0100

    Fix typo with scrolloff

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
     vim.opt_local.expandtab = true
-    vim.opt.scrollof = 8
+    vim.opt.scrolloff = 8
   end,
 })
 
@@ -23,6 +23,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.shiftwidth = 4
     vim.opt_local.expandtab = true
     vim.opt_local.textwidth = 80
-    vim.opt.scrollof = 8
+    vim.opt.scrolloff = 8
   end,
 })
