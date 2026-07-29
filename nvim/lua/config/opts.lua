25b9b41250e85da02f497b2438265d5ecba25bda
Oscar Sellors <omsellors98@gmail.com>
2026-07-29 20:16:40 +0100

    move lazynvim to its own folder and start my home-grown config

---
+++ nvim/lua/config/opts.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.opt.number = true           -- absolute numbers
+vim.opt.relativenumber = true   -- relative numbers
+vim.opt.numberwidth = 4         -- room for 4 digits in the gutter
+
+vim.opt.tabstop = 4         -- <tab> displays as 4 spaces 
+vim.opt.shiftwidth = 4      -- auto-indent to 4 spaces
+vim.opt.softtabstop = 4     -- spaces inserted/removed per <tab>/<bs>
+vim.opt.expandtab = true    -- convert tabs to spaces
