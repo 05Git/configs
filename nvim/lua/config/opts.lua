3ca0d9da1ddb3923b9bbd331f64052039cdffb13
Oscar Sellors <omsellors98@gmail.com>
2026-08-12 22:30:12 +0100

    netrw default style = trees now

--- nvim/lua/config/opts.lua
+++ nvim/lua/config/opts.lua
@@ -1,13 +1,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.g.netrw_liststyle = 3 -- tree-style netrw dir listing
 
-vim.opt.number = true           -- absolute numbers
-vim.opt.relativenumber = true   -- relative numbers
-vim.opt.numberwidth = 4         -- room for 4 digits in the gutter
+vim.opt.number = true -- absolute numbers
+vim.opt.relativenumber = true -- relative numbers
+vim.opt.numberwidth = 4 -- room for 4 digits in the gutter
 
-vim.opt.tabstop = 4         -- <tab> displays as 4 spaces 
-vim.opt.shiftwidth = 4      -- auto-indent to 4 spaces
-vim.opt.softtabstop = 4     -- spaces inserted/removed per <tab>/<bs>
-vim.opt.expandtab = true    -- convert tabs to spaces
+vim.opt.tabstop = 4 -- <tab> displays as 4 spaces
+vim.opt.shiftwidth = 4 -- auto-indent to 4 spaces
+vim.opt.softtabstop = 4 -- spaces inserted/removed per <tab>/<bs>
+vim.opt.expandtab = true -- convert tabs to spaces
 
-vim.opt.scrolloff = 8       -- minimum 8 lines above/below cursor
+vim.opt.scrolloff = 8 -- minimum 8 lines above/below cursor
