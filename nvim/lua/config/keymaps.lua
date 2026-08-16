8eb2f0c6f0e0720fe580421c66034e9d7cfab980
Oscar Sellors <omsellors98@gmail.com>
2026-08-16 15:44:07 +0100

    Update to use mini.files over netrw, and also disable lua.rocks

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,1 +1,8 @@
-vim.keymap.set("n", "<Leader>pv", vim.cmd.Ex, { desc = "Open ntrw file explorer" })
+-- explore buffer dir
+vim.keymap.set("n", "<Leader>eb", function()
+	require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+end, { desc = "Open current buffer dir w/mini.files" })
+-- explore pwd
+vim.keymap.set("n", "<Leader>ep", function()
+	require("mini.files").open(vim.fn.getcwd(), true)
+end, { desc = "Open current pwd w/mini.files" })
