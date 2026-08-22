43c047b74bc18af55f623f73c4adb77cddad7282
Oscar Sellors <omsellors98@gmail.com>
2026-08-22 20:48:59 +0100

    lsp keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,15 @@
 -- explore buffer dir
 vim.keymap.set("n", "<Leader>eb", function()
-  require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+	require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
 end, { desc = "Open current buffer dir w/mini.files" })
 -- explore pwd
 vim.keymap.set("n", "<Leader>ep", function()
-  require("mini.files").open(vim.fn.getcwd(), true)
+	require("mini.files").open(vim.fn.getcwd(), true)
 end, { desc = "Open current pwd w/mini.files" })
+
+-- lsp
+vim.keymap.set("n", "grd", vim.lsp.buf.definition, { desc = "LSP Definition" })
+vim.keymap.set("n", "grs", vim.lsp.buf.signature_help, { desc = "LSP Signature Help" })
+vim.keymap.set("n", "grT", vim.lsp.buf.typehierarchy, { desc = "LSP Type-Hierarchy" })
+vim.keymap.set("n", "grS", vim.lsp.buf.workspace_symbol, { desc = "LSP Workspace Symbols" })
+vim.keymap.set("n", "grD", vim.lsp.buf.workspace_diagnostics, { desc = "LSP Workspace Diagnostics" })
