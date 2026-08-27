5147538180838e01b4948112f5dd8fa5fcdc42fb
05Git <omsellors98@gmail.com>
2026-08-27 21:52:20 +0100

    mocha looks cooler on windows

--- nvim/lua/config/plugins/color.lua
+++ nvim/lua/config/plugins/color.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000,
     config = function()
       require("catppuccin").setup({
-        flavour = "frappe", -- latte = light, frappe/macchiato/mocha = dark
+        flavour = "mocha", -- latte = light, frappe/macchiato/mocha = dark
         no_italic = true, -- annnoying to look at
         integrations = {
           nvimtree = true,
